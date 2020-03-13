688d4e8c9b2bebd4e088c7129be2f89caf68491c
Clayton O'Neill <claytononeill@Claytons-MacBook-Pro.local>
2020-03-13 14:31:22 -0400

    the show route and the delete function are up and running

--- controllers/beer.js
+++ controllers/beer.js
@@ -3,10 +3,26 @@ const router = express.Router();
 const Beer = require('../models/beermodel.js');
 
 
+router.get('/:id', (req,res) => {
+  Beer.findById(req.params.id, (error, foundBeer) => {
+    res.render('BeerMain/show.ejs',
+    {
+      thisBeer: foundBeer
+    })
+  })
+})
+
+
+router.delete('/:id', (req,res) => {
+  Beer.findByIdAndRemove(req.params.id, (error,removedEntry) => {
+    res.redirect('/The-Beer-Cellar')
+  })
+})
+
+
 router.get('/new', (req,res) => {
   res.render('BeerMain/new.ejs')
 })
-
 
 
 
